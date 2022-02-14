b3b0c27d475a327a6cfeac7ff18c0351c4e86c93
Sean Johnson <sean@skj.dev>
2022-02-14 17:49:26 -0600

    adding background transcoding job using software x264 CBR

---
+++ plex/transcoding/transcode.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Random micro sleep to mitigate simultaneous startup race condition.
+sleep .${RANDOM:1:2}
+
+shopt -s nullglob
+FILES=(/plex/originals/inbox/*.mkv)
+shopt -u nullglob
+
+if [ "${#FILES}" -ne 0 ]; then
+
+  for SOURCE in "${FILES[@]}"; do
+
+    if [ -f "${SOURCE%.mkv}.inprogress"]; then
+      continue
+    fi
+
+    touch "${SOURCE%.mkv}.inprogress"
+
+    cd $(mktemp -d -p /tmp/transcode)
+    /usr/local/bin/classic-transcode "${SOURCE}"
+
+    if [ "$?" -eq 0 ]; then
+      mv "${SOURCE##*/}" /plex/rips/movies/
+      mv "${SOURCE}" /plex/originals/movies/
+      rm "${SOURCE%.mkv}.inprogress"
+    else
+      echo "something bad happened!"
+      exit 1
+    fi
+
+  done
+
+fi
+
+
+
