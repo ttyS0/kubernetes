5cb4c3e388a3c873c98136b7f8652f66c943f032
Sean Johnson <sean@skj.dev>
2022-02-17 09:35:00 -0600

    added comments to transcode.sh, and added an else to exit if the duration doesn't check out at the end

--- plex/transcoding/transcode.sh
+++ plex/transcoding/transcode.sh
@@ -11,17 +11,20 @@ if [ "${#FILES}" -ne 0 ]; then
     # Random micro sleep to mitigate simultaneous startup race condition.
     sleep .${RANDOM:1:2}
 
+    # Find a file that isn't already in progress
     if [ -f "${SOURCE%.mkv}.inprogress" ] || [ ! -f "${SOURCE}" ]; then
       continue
     fi
 
+    # Claim the file
     touch "${SOURCE%.mkv}.inprogress"
 
+    # Setup workspace on local storage
     cd $(mktemp -d -p /tmp/transcode)
     mkdir src
     cp "${SOURCE}" src/
 
-    # Check size
+    # Verify the copied source file
     ORIG=$(wc -c "${SOURCE}" | cut -d' ' -f1)
     NEW=$(wc -c "src/${SOURCE##*/}" | cut -d' ' -f1)
 
@@ -31,15 +34,21 @@ if [ "${#FILES}" -ne 0 ]; then
       exit 1
     fi
 
+    # Transcode!
     /usr/local/bin/classic-transcode "src/${SOURCE##*/}"
 
+    # The exit code can't be trusted, so do a sanity check based on duration
     ORIG=$(/usr/local/bin/ffprobe "src/${SOURCE##*/}" | awk '/Duration/ { print $2 }' | cut -d'.' -f1)
     NEW=$(/usr/local/bin/ffprobe "${SOURCE##*/}" | awk '/Duration/ { print $2 }' | cut -d'.' -f1)
 
+    # If the duration checks out, copy the transcode to Plex, and clean up
     if [ "${NEW}" == "${ORIG}" ]; then
       cp "${SOURCE##*/}" /plex/rips/movies/
       mv "${SOURCE}" /plex/originals/movies/
       rm "${SOURCE%.mkv}.inprogress"
+    else
+      echo "something went wrong!"
+      exit 1
     fi
 
   done
