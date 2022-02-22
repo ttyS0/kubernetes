77b87f17671e58d2a1acdea66284717b2ffc5583
Sean Johnson <sean@skj.dev>
2022-02-21 20:55:06 -0600

    updated transcode.sh to work on in-place Plex library
    added extra progress information
    added processing of TV Shows
    bumped container to other-transcode:sw-0.10.0-5, which uses FDK-AAC VBR

--- plex/transcoding/transcode.sh
+++ plex/transcoding/transcode.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-shopt -s nullglob
-FILES=(/plex/originals/inbox/*.mkv)
-shopt -u nullglob
+FILES=(/plex/rips/movies/*.mkv /plex/rips/series/*/*.mkv)
 
 if [ "${#FILES}" -ne 0 ]; then
 
@@ -11,44 +9,69 @@ if [ "${#FILES}" -ne 0 ]; then
     # Random micro sleep to mitigate simultaneous startup race condition.
     sleep .${RANDOM:1:2}
 
-    # Find a file that isn't already in progress
-    if [ -f "${SOURCE%.mkv}.inprogress" ] || [ ! -f "${SOURCE}" ]; then
+    # Find a file that isn't already in progress or transcoded
+    if [ -f "${SOURCE%.mkv}.inprogress" ] || [ -f "${SOURCE%.mkv}.transcoded" ] || [ ! -f "${SOURCE}" ]; then
       continue
     fi
 
     # Claim the file
     touch "${SOURCE%.mkv}.inprogress"
 
-    # Setup workspace on local storage
+    # Setup a temporary workspace on local storage
     cd $(mktemp -d -p /tmp/transcode)
     mkdir src
+
+    # Copy the source to the local system
+    echo "Copying ${SOURCE##*/}..."
     cp "${SOURCE}" src/
 
     # Verify the copied source file
+    echo "Calculating ckecksums ..."
     ORIG=$(md5sum "${SOURCE}" | cut -d' ' -f1)
     NEW=$(md5sum "src/${SOURCE##*/}" | cut -d' ' -f1)
 
     if [ "${NEW}" != "${ORIG}" ]; then
-      echo "file mismatch!"
+      echo "Checksum mismatch!"
+      echo "ORIG = ${ORIG}"
+      echo "NEW = ${NEW}"
       rm "${SOURCE%.mkv}.inprogress"
       exit 1
     fi
 
     # Transcode!
+    echo "Transcoding ${SOURCE##*/} ..."
     /usr/local/bin/classic-transcode "src/${SOURCE##*/}"
 
     # The exit code can't be trusted, so do a sanity check based on duration
+    echo "Performing duration check ..."
     ORIG=$(/usr/local/bin/ffprobe "src/${SOURCE##*/}" 2>&1 | awk '/Duration/ { print $2 }' | cut -d'.' -f1)
     NEW=$(/usr/local/bin/ffprobe "${SOURCE##*/}" 2>&1 | awk '/Duration/ { print $2 }' | cut -d'.' -f1)
 
-    # If the duration checks out, copy the transcode to Plex, and clean up
+    # If the duration checks out, backup the original,
+    #   copy the transcode to the Plex library, and clean up
     if [ "${NEW}" == "${ORIG}" ]; then
-      cp "${SOURCE##*/}" /plex/rips/movies/
-      mv "${SOURCE}" /plex/originals/movies/
+      # If original's parent directory doesn't exist, create it.
+      DIR="${SOURCE%/*}"
+      if [ ! -d "${DIR/rips/originals}" ]; then
+        mkdir -p "${DIR/rips/originals}"
+      fi
+
+      echo "Moving ${SOURCE##*/} to originals ..."
+      mv "${SOURCE}" "${SOURCE/rips/originals}"
+
+      echo "Placing transcoded ${SOURCE##*/} into library ..."
+      cp "${SOURCE##*/}" "${SOURCE}"
+
+      echo "Marking as complete ..."
       rm "${SOURCE%.mkv}.inprogress"
+      touch "${SOURCE%.mkv}.transcoded"
+
+      echo "Cleaning up tmp space ..."
       rm -rf /tmp/transcode/tmp.*
     else
-      echo "something went wrong!"
+      echo "Transcode duration (${NEW}) doesn't match source duration (${ORIG})!"
+      echo "Exiting!"
+      rm "${SOURCE%.mkv}.inprogress"
       exit 1
     fi
 
